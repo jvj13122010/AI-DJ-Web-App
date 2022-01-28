6c3b21b5fd9e55fe0ac717121eb8b898b216091f
jvj13122010 <80154440+jvj13122010@users.noreply.github.com>
2022-01-28 12:55:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-sound = "";
-
+sound = [];
+counter = 0;
 function preload(){
-    sound = loadSound("music.mp3");
+    sound.push(loadSound("SO69.mp3"));
+    sound.push(loadSound("music.mp3"));
+    sound.push(loadSound("JamesB_UB.mp3"));
+    sound.push(loadSound("AOM.mp3"));
+    sound.push(loadSound("CGC.mp3"));
+    sound.push(loadSound("DSD.mp3"));
+    sound.push(loadSound("ETD.mp3"));
+    sound.push(loadSound("Heer.mp3"));
+    sound.push(loadSound("KahinTo.mp3"));
+    sound.push(loadSound("KHNH.mp3"));
+    sound.push(loadSound("MH.mp3"));
+    sound.push(loadSound("Ranjha.mp3"));
+    sound.push(loadSound("SLU.mp3"));
+    sound.push(loadSound("THH.mp3"));
+    sound.push(loadSound("THR.mp3"));
+    sound.push(loadSound("TJNK.mp3"));
+    sound.push(loadSound("TKCL.mp3"));
+    sound.push(loadSound("tumSe.mp3"));
+
 }
 
 function play(){
-    sound.play();
+    sound[counter].play();
+    sound.setVolume(1);
+    sound.rate(1);
 }
 
 function pause(){
-    sound.pause();
+    sound[counter].pause();
 }
 
 function stopSound(){
-    sound.stop();
+    sound[counter].stop();
 }
 
 function setup(){
@@ -25,4 +45,23 @@ video.hide();
 
 function draw(){
     image(video,0,0,600,500);
+}
+
+function next(){
+sound[counter].stop();
+    counter+=1;
+if (counter==sound.length) {
+    counter = 0;
+}
+play();
+}
+
+function prev(){
+    sound[counter].stop();
+    counter-=1;
+    if (counter == -1) {
+        counter = 2;
+    }
+    play();
+
 }
